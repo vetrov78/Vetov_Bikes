c8bf71c6c360816b4fab2a9af74596f6ee70bb78
Andrei Vetrov <vettrov78@gmail.com>
2021-09-26 10:14:39 +0300

    На проверку наставнику

--- build/js/main.js
+++ build/js/main.js
@@ -1,38 +1,40 @@
 'use strict';
-var pageHeaderNav = document.querySelector('.page-header__navigation');
-var headerToggle = document.querySelector('.page-header__navigation-toggle');
+(function () {
+  var pageHeaderNav = document.querySelector('.page-header__navigation');
+  var headerToggle = document.querySelector('.page-header__navigation-toggle');
 
-pageHeaderNav.classList.remove('page-header__navigation--nojs');
-pageHeaderNav.classList.add('page-header__navigation--closed');
-pageHeaderNav.classList.remove('page-header__navigation--opened');
+  pageHeaderNav.classList.remove('page-header__navigation--nojs');
+  pageHeaderNav.classList.add('page-header__navigation--closed');
+  pageHeaderNav.classList.remove('page-header__navigation--opened');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeaderNav.classList.contains('page-header__navigation--closed')) {
-    pageHeaderNav.classList.remove('page-header__navigation--closed');
-    pageHeaderNav.classList.add('page-header__navigation--opened');
-  } else {
-    pageHeaderNav.classList.add('page-header__navigation--closed');
-    pageHeaderNav.classList.remove('page-header__navigation--opened');
-  }
-});
-
-// Закрытие мобильного меню после выбора любого пункта
-var menuItem = document.querySelectorAll('.page-header__navigation-item');
-
-for (var i = 0; i < menuItem.length; i++) {
-  menuItem[i].addEventListener('click', function () {
-    pageHeaderNav.classList.add('page-header__navigation--closed');
-    pageHeaderNav.classList.remove('page-header__navigation--opened');
+  headerToggle.addEventListener('click', function () {
+    if (pageHeaderNav.classList.contains('page-header__navigation--closed')) {
+      pageHeaderNav.classList.remove('page-header__navigation--closed');
+      pageHeaderNav.classList.add('page-header__navigation--opened');
+    } else {
+      pageHeaderNav.classList.add('page-header__navigation--closed');
+      pageHeaderNav.classList.remove('page-header__navigation--opened');
+    }
   });
-}
 
-var phoneInput = document.querySelector('#phone');
-phoneInput.addEventListener('input', function () {
-  var regex = /\d+/g;
-  if (!regex.test(phoneInput.value)) {
-    phoneInput.setCustomValidity('Только цифры');
-  } else {
-    phoneInput.setCustomValidity('');
+  // Закрытие мобильного меню после выбора любого пункта
+  var menuItem = document.querySelectorAll('.page-header__navigation-item');
+
+  for (var i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener('click', function () {
+      pageHeaderNav.classList.add('page-header__navigation--closed');
+      pageHeaderNav.classList.remove('page-header__navigation--opened');
+    });
   }
-  phoneInput.reportValidity();
-});
+
+  var phoneInput = document.querySelector('#phone');
+  phoneInput.addEventListener('input', function () {
+    var regex = /^\d+$/;
+    if (!regex.test(phoneInput.value)) {
+      phoneInput.setCustomValidity('Только цифры');
+    } else {
+      phoneInput.setCustomValidity('');
+    }
+    phoneInput.reportValidity();
+  });
+})();
